763ab83145ad0da8009ad235f567a078340b0310
gstrehlow <90304692+gstrehlow@users.noreply.github.com>
2022-01-29 17:22:23 -0600

    setup the indexedDB for offline functionality

--- public/service-worker.js
+++ public/service-worker.js
@@ -12,7 +12,7 @@ const FILES_TO_CACHE = [
 self.addEventListener("install", function (e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function (cache) {
-      console.log("installing cache : " + CACHE_NAME);
+      console.log("Files cached successfully!");
       return cache.addAll(FILES_TO_CACHE);
     })
   );
